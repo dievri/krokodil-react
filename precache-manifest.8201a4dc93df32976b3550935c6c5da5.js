02160532f1dbaaaf7c55f72f3d89cf4d9ab3f3e3
Oleksandr Dievri <o.dievri@outlook.com>
2019-02-18 18:40:15 +0200

    Updates

---
+++ precache-manifest.8201a4dc93df32976b3550935c6c5da5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3adb6f66f1be1ccc93a6",
+    "url": "/krokodil-react/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "3adb6f66f1be1ccc93a6",
+    "url": "/krokodil-react/static/js/main.3adb6f66.chunk.js"
+  },
+  {
+    "revision": "6550dae9f96e4ea0c874",
+    "url": "/krokodil-react/static/js/runtime~main.6550dae9.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/krokodil-react/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "a025252298e3b5afd64ba1b86298153c",
+    "url": "/krokodil-react/index.html"
+  }
+];
